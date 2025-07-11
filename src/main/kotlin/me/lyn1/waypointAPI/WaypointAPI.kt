4dab1828d798f406ec44d832dffc0799906df70f
lyn1dev <chosentaco68@gmail.com>
2025-07-12 00:26:15 +0800

    init commit

---
+++ src/main/kotlin/me/lyn1/waypointAPI/WaypointAPI.kt
@@ -0,0 +1,14 @@
+package me.lyn1.waypointAPI
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class WaypointAPI : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
